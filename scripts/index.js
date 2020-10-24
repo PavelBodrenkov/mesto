cedc98d624e0cec9b39b1bab3dc84adc2b160163
Pavel Bodrenkov <pbodrenkov@yandex.ru>
2020-10-24 11:27:48 +0300

    Третья работа 1.6

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,44 @@
-let popup = document.querySelector('.popup');
-let buttonClose = popup.querySelector('.popup__button-close');
-let profileInfo = document.querySelector('.profile__info');
-let editButton = document.querySelector('.profile__edit-button');
-let profileName = document.querySelector('.profile__name');
-let profileSubtitle = document.querySelector('.profile__subtitle');
+const popup = document.querySelector('.popup');
+const buttonClose = popup.querySelector('.popup__button-close');
+const profileInfo = document.querySelector('.profile__info');
+const editButton = document.querySelector('.profile__edit-button');
+const profileName = document.querySelector('.profile__name');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+const formElement = document.querySelector('.form');
+const nameInput = formElement.querySelector('.popup__data');
+const jobInput = formElement.querySelector('.popup__name');
 
-//Открываем попап
-editButton.addEventListener('click', openForm);
+//добавляем данные в value со страницы
+function setInputValue() {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileSubtitle.textContent;
+}
+
 function openForm() {
     popup.classList.add('popup_opened');
+    setInputValue();
 }
 
-//Закрываем попап
-buttonClose.addEventListener('click', closeForm);
 function closeForm(){
-    popup.classList.remove('popup_opened');
+    popup.classList.remove('popup_opened'); 
 }
-
-//Функция закрывает форму клик по фону
+//Функция закрывает форму клик по фону//
 function popupClickHandler(event) {
     if(event.target.classList.contains('popup')) {
         closeForm();
     }
 }
 
-let formElement = document.querySelector('.form');
-function formSubmitHandler (evt) {
-    evt.preventDefault();//убираем базовый функционал кнопки
-    let nameInput = formElement.querySelector('.popup__data');
-    let jobInput = formElement.querySelector('.popup__name');
+function formSubmitHandler(evt) {
+    evt.preventDefault();
     profileName.textContent=nameInput.value;
     profileSubtitle.textContent=jobInput.value;
+    closeForm();
 }
 
+//Открываем попап//
+editButton.addEventListener('click', openForm);
+//Закрываем попап//
+buttonClose.addEventListener('click', closeForm);
 formElement.addEventListener('submit', formSubmitHandler); 
-formElement.addEventListener('submit', closeForm);
 popup.addEventListener('click', popupClickHandler);
